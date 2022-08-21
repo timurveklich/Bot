97e84c47527fdc29f2a2d2caf9dd03d764c23649
Timur Veklich <106811829+timurveklich@users.noreply.github.com>
2022-08-21 12:23:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# token can be retrieved with 
+# token can be retrieved with @BotFather
 TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 # group ids or account ids can be retrieved with @username_to_id_bot
