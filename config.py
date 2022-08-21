6f4144c6f6e9b25e9cf46eb03f65c08315f6e909
Timur Veklich <106811829+timurveklich@users.noreply.github.com>
2022-08-21 12:11:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-# group ids or account ids can be retrieved with @username_to_id_bot
+# token can be retrieved with 
 TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+# group ids or account ids can be retrieved with @username_to_id_bot
 ID = 1111111111
